b09c925bfa6f25837ed774be2bc5055c4eba4406
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-08-26 14:39:36 +0530

    First assignment of OS course
    This is the first assignment of OS course.This is the part of Refresher course.
    This basically takes you through different parts of compilation of code.

---
+++ RM_A_0_1/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//adding comment which must be removed in preprocessing step
+
+int main()
+
+{
+
+	int a=10;
+
+	int b=20;
+
+	printf("value of a is %d and of b is %d \n", a , b);
+
+	return 0;
+
+}
+
---
+++ RM_A_0_1/hello.s
@@ -0,0 +1,35 @@
+	.file	"hello.c"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"value of a is %d and of b is %d \n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$10, -8(%rbp)
+	movl	$20, -4(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
