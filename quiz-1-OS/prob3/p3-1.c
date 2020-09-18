05ec5ae23c3f21bad0fce62186f51a6011e1d1af
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-09-18 22:44:14 +0530

    Add files via upload

---
+++ quiz-1-OS/prob3/p3-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char x[64];
+    long int sto[8];
+    sto[0]=122;
+    sto[1]=121;
+    sto[2]=126;
+    sto[3]=125;
+    sto[4]=124;
+    sto[5]=123;
+    sto[6]=122;
+    sto[7]=121;
+
+    for(int i=0;i<8;i++)
+    {
+        memcpy(&x[i*8], &sto[i], 8);
+    }
+
+    return 0;
+}
---
+++ quiz-1-OS/prob3/p3-2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    
+    // printf("size of int %ld\n",sizeof(int));
+    // printf("size of long int %ld\n",sizeof(long int));
+    // printf("pointer value of li is %p\n",pli);   
+
+    char x[64];
+    int sto[16];
+    sto[0]=122;
+    sto[1]=121;
+    sto[2]=126;
+    sto[3]=125;
+    sto[4]=124;
+    sto[5]=123;
+    sto[6]=122;
+    sto[7]=121;
+    sto[8]=22;
+    sto[9]=21;
+    sto[10]=26;
+    sto[11]=25;
+    sto[12]=24;
+    sto[13]=23;
+    sto[14]=22;
+    sto[15]=21;
+
+    for(int i=0;i<16;i++)
+    {
+        memcpy(&x[i*4], &sto[i], 4);
+    }
+    // x[0]= i  & 0xff;
+    // x[1]= (i>>8)   & 0xFF;
+    // x[2]= (i>>16)  & 0xFF;
+    // x[3]= (i>>24)  & 0xFF;
+    // printf("%d\n", x[0]);
+    // printf("%d\n", x[1]);
+    // printf("%d\n", x[2]);
+    // printf("%d\n", x[3]);
+
+    //memcpy(&x[4],&j,4);
+    // printf("%d\n", x[4]);
+    // printf("%d\n", x[5]);
+    // printf("%d\n", x[6]);
+    // printf("%d\n", x[7]);
+
+    printf("run successful\n");
+    return 0;
+}
