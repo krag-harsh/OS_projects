c8ccfb6b22d0bbb2924e2073625e695f537271bd
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-09-29 13:29:53 +0530

    cat command added

--- Assignment-1/q2/cat.c
+++ Assignment-1/q2/cat.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char *argv[])
 {
-    printf("We are in cat.c\n");
-    printf("PID of try.c = %d\n", getpid());
-    int i=0;
-    printf("value of int argc passed= %d \n",argc);
-    for(i=0;i<argc;i++)
+    //printf("We are in cat.c\n");
+    if(argc==3)
     {
-        printf("%s\n",argv[i]);
+        if(strcmp(argv[1],"-E")==0)
+        {
+            FILE* stream = fopen(argv[2], "r");
+            char line[1024];
+            while (fgets(line, 1024, stream))
+            {
+                char* tmp = strdup(line);
+                //printf("%s",tmp);
+                for(int i=0;i<strlen(tmp);i++)
+                {
+                    if(tmp[i]=='\n')
+                    printf("$");
+                    printf("%c",tmp[i]);
+                }
+                free(tmp);
+            }
+
+        }
+
+        else if(strcmp(argv[1],"-n")==0)
+        {
+            FILE* stream = fopen(argv[2], "r");
+            int count=0;
+            char line[1024];
+            while (fgets(line, 1024, stream))
+            {
+                char* tmp = strdup(line);
+                count++;
+                printf("\t%d  %s",count,tmp);
+                free(tmp);
+            }
+
+        }
+        
+
     }
+    else if (argc==2)
+    {
+        FILE* stream = fopen(argv[1], "r");
+        char line[1024];
+        while (fgets(line, 1024, stream))
+        {
+            char* tmp = strdup(line);
+            printf("%s",tmp);
+            free(tmp);
+        }
+    }
+    
     return 0;
 }
