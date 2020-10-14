bb983eee4e9173878a95f16974b3f2d3d4acc52c
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-10-14 13:51:37 +0530

    Add files via upload

--- Assignment-2/Q1/Q1_part1.c
+++ Assignment-2/Q1/Q1_part1.c
@@ -1,2 +1,46 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int n=10;      //global variable
+
+
+int main()
+{
+   printf("hi\n");
+   pid_t pid;
+	pid=fork();
+
+   // pid_t pid=fork();
+
+	if(pid<0){
+		fprintf(stderr, "Ford Filed");
+	}
+	else if(pid ==0)
+	{
+		printf("In child process\n");
+      while(n>-90)
+      {
+         n--;
+         printf("%d\n",n);
+      }
+   }
+   else
+   {
+      waitpid(-1, NULL , 0);
+      printf("Inside parent process\n");
+      while (n<100)
+      {
+         n++;
+         printf("%d\n",n);
+      }
+   }
+
+
+   return 0;
+}
+   
--- Assignment-2/Q1/Q1_part2.c
+++ Assignment-2/Q1/Q1_part2.c
@@ -1,2 +1,44 @@
 /* Name: Your Name
    Roll_Number: Your Roll_Number */
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h> 
+
+int n=10;
+
+void *workinthread(void *arg)
+{
+   printf("In child process\n");
+      while(n>-90)
+      {
+         n--;
+         printf("%d\n",n);
+      }
+
+}
+
+
+int main()
+{
+
+
+   pthread_t newthread;
+   pthread_create(&newthread, NULL, workinthread, NULL);
+   //pthread_exit(NULL);
+
+   printf("Inside parent process\n");
+   while (n<100)
+   {
+      n++;
+      printf("%d\n",n);
+   }
+
+   pthread_join(newthread,NULL);
+   
+   return 0;
+}
