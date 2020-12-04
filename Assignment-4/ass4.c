365fb4b487645dc16dc6589a37a2b20ad4b25f65
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-12-04 11:25:27 +0530

    Add files via upload

--- Assignment-4/ass4.c
+++ Assignment-4/ass4.c
@@ -14,75 +14,81 @@ struct my_semaphore{
 	int val;
 };
 
-// All the methods for my_semaphore struct only
-// This will initialize the semaphore by initializing 
+void wait(struct my_semaphore *sem)
+{
+	// pthread_mutex_lock(&sem->mutex);
+	// while(sem->val == 0)
+	// 	pthread_cond_wait(&sem->cond, &sem->mutex);	//wait karwayega process ko
+	// sem->val--;
+	// pthread_mutex_unlock(&sem->mutex);
 
-// void sem_INIT(struct my_semaphore *sem, int value){
-// 	pthread_mutex_init(&sem->mutex, NULL);			
-// 	pthread_cond_init(&sem->cond, NULL);
-// 	sem->val = value;
-// }
-
-void wait(struct my_semaphore *sem){
 	pthread_mutex_lock(&sem->mutex);
-	while(sem->val == 0){
-		pthread_cond_wait(&sem->cond, &sem->mutex);	//wait karwayega process ko
-	}
-	// printf("In wait with val : %d\n", sem->val);
 	sem->val--;
+	if(sem->val < 0)
+		pthread_cond_wait(&sem->cond, &sem->mutex);	//wait karwayega process ko
 	pthread_mutex_unlock(&sem->mutex);
 }
 
-void signal(struct my_semaphore *sem){
+void signal(struct my_semaphore *sem)
+{
+	// pthread_mutex_lock(&sem->mutex);
+	// sem->val++;
+	// pthread_cond_signal(&sem->cond);
+	// pthread_mutex_unlock(&sem->mutex);
 	pthread_mutex_lock(&sem->mutex);
-	// printf("In signal with val : %d\n", sem->val);    
 	sem->val++;
-	pthread_cond_signal(&sem->cond);
+	if(sem->val <=0)
+	pthread_cond_signal(&sem->cond);	//wait karwayega process ko
 	pthread_mutex_unlock(&sem->mutex);
+}
+
+void PrintValue(struct my_semaphore *s)
+{
+	printf("%d\n", s->val);
 }
 
 
 struct helperStruct{
 
-	struct my_semaphore* helper_room;
-	struct my_semaphore* helper_bowl;
+	// struct my_semaphore* helper_room;
+	// struct my_semaphore* helper_bowl;
 	struct my_semaphore* helper_fork;
 	struct my_semaphore* helper_forknext;
 	int helper_philoNumber;
 
 };
-//  Method in which philosophers eat and then returns stuff
+
+// struct my_semaphore room;
+struct my_semaphore bowl;		// Pair of bowl is taken as one object because both the bowls is needed at a time
+
 void* work(void* helper_var){
 	
 	int num = (*(struct helperStruct*)helper_var).helper_philoNumber;
-
-	struct my_semaphore *room = ((struct helperStruct*)helper_var)->helper_room;
 	struct my_semaphore *fork = ((struct helperStruct*)helper_var)->helper_fork;
-	struct my_semaphore *bowl = ((struct helperStruct*)helper_var)->helper_bowl;
 	struct my_semaphore *forknext = ((struct helperStruct*)helper_var)->helper_forknext;
 	
-	wait(room);
-	wait(fork);
-	wait(forknext);
-	wait(bowl);
+	while(1)
+	{
+		//wait(&room);
+		wait(&bowl);
+		wait(fork);
+		wait(forknext);
+		
+		printf("Philosopher %d eats using forks %d and %d\n",num, num,(num+1)%numPhilo);
+		sleep(1);
 
-	printf("Philosopher %d eats using forks %d and %d\n",num, num,(num+1)%numPhilo);
-	sleep(1);
-
-	signal(forknext);
-	signal(fork);
-	signal(bowl);
-	signal(room);
-	return NULL;
+		signal(forknext);
+		signal(fork);
+		signal(&bowl);
+		//signal(&room);
+	}
+	
+	// PrintValue(fork);
 
 }
 
 int main(int argc, char const *argv[])
 {	
-	struct my_semaphore room;
-
-	// Pair of bowl is taken as one object because both the bowls is needed at a time
-	struct my_semaphore bowl;
 
 	printf("Enter the number of Philosophers\n");
 	scanf("%d",&numPhilo);
@@ -94,14 +100,12 @@ int main(int argc, char const *argv[])
 
 	pthread_t tid[numPhilo];
 	
-	// int philosophers[numPhilo];
-
 	//  In order to avoid deadlocks, n-1 philosophers should be allowed in a room at once
 	// sem_INIT(&room, numPhilo-1);
 
-	pthread_mutex_init(&(&room)->mutex, NULL);			
-	pthread_cond_init(&(&room)->cond, NULL);
-	(&room)->val = numPhilo-1;
+	// pthread_mutex_init(&(&room)->mutex, NULL);			
+	// pthread_cond_init(&(&room)->cond, NULL);
+	// (&room)->val = numPhilo-1;
 
 	// A pair of bowl is needed for eating and when eating is done both the bowls are left therefore we are creating a single bowl.under the assumption that they pick both bowl very fast.
 	// sem_INIT(&bowl, 1);
@@ -109,22 +113,22 @@ int main(int argc, char const *argv[])
 	pthread_cond_init(&(&bowl)->cond, NULL);
 	(&bowl)->val = 1;
 
+	// helper[i].helper_bowl = &bowl;
+	// helper[i].helper_room = &room;
+
 
 	for(int i=0; i<numPhilo; i++){
-		// sem_INIT(&forks[i],1);
+
 		pthread_mutex_init(&(&forks[i])->mutex, NULL);
 		pthread_cond_init(&(&forks[i])->cond, NULL);
 		(&forks[i])->val = 1;
 
 		helper[i].helper_fork = &forks[i];
 		helper[i].helper_forknext = &forks[(i+1)%numPhilo];
-		helper[i].helper_bowl = &bowl;
-		helper[i].helper_room = &room;
 	}
 
 	for(int i=0; i<numPhilo; i++){
-		// philosophers[i] = i;	
-		helper[i].helper_philoNumber = i;																// For checking the baju vala banda //
+		helper[i].helper_philoNumber = i;				// For checking the baju vala banda //
 		pthread_create(&tid[i],NULL, &work, (void*)&helper[i]);
 	}
 
